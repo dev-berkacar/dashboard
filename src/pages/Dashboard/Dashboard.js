8f21caf7b2b4ef9b1a9f45c248d968be8469964e
berk <dev.berkacar@gmail.com>
2022-08-08 23:08:27 +0300

    fix

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -55,14 +55,12 @@ export default function Dashboard(props) {
   };
 
   function inputOnKeyUp() {
-    // Declare variables
-    var input, filter, table, tr, td, i, txtValue;
+    let input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
     table = document.getElementById("myTable");
     tr = table.getElementsByTagName("tr");
 
-    // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
       td = tr[i].getElementsByTagName("td")[parseInt(dropdownValue)];
       if (td) {
